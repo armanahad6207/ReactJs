f32071fd283999e75e9a014c123ea77a83100486
armanahad6207 <armanahad7549@gmail.com>
2024-02-27 10:22:48 +0530

    add useState

--- TICTACTOE-VITE/src/App.jsx
+++ TICTACTOE-VITE/src/App.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './style.scss';
 import Board from './components/Board';
 import Square from './components/Square';
-import Heloo from './components/Heloo';
 
 const App = () => {
+  let [counter, setCounter] = useState(1);
+  const onBtnClick = () => {
+    console.log('updating curen values');
+    setCounter(curentValues => {
+      return (curentValues += 1);
+    });
+  };
   return (
     <div className="app">
-      <Board />
+      <div>
+        <button onClick={onBtnClick}>click me</button>
+        <h1>{counter}</h1>
+      </div>
     </div>
   );
 };
