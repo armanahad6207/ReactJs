e99aade53697f933beefaac8095a0e3acc560321
armanahad6207 <armanahad7549@gmail.com>
2024-02-28 08:21:11 +0530

    create game board state

--- TICTACTOE-VITE/src/App.jsx
+++ TICTACTOE-VITE/src/App.jsx
@@ -4,19 +4,10 @@ import Board from './components/Board';
 import Square from './components/Square';
 
 const App = () => {
-  let [counter, setCounter] = useState(1);
-  const onBtnClick = () => {
-    console.log('updating curen values');
-    setCounter(curentValues => {
-      return (curentValues += 1);
-    });
-  };
   return (
     <div className="app">
-      <div>
-        <button onClick={onBtnClick}>click me</button>
-        <h1>{counter}</h1>
-      </div>
+      <Board />
+      <Square />
     </div>
   );
 };
--- TICTACTOE-VITE/src/components/Board.jsx
+++ TICTACTOE-VITE/src/components/Board.jsx
@@ -1,23 +1,45 @@
+import { useState } from 'react';
 import Square from './Square';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleSquareClick = clickedPosition => {
+    setSquares(curentSquares => {
+      return curentSquares.map((squaresValue, position) => {
+        console.log(clickedPosition, position);
+        if (clickedPosition === position) {
+          return 'X';
+        }
+        return squaresValue;
+      });
+    });
+  };
+  const renderSquare = position => {
+    return (
+      <Square
+        value={squares[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
+
   return (
     <>
       <div className="board">
         <div className="board-row">
-          <Square value={0} />
-          <Square value={2} />
-          <Square value={3} />
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className="board-row">
-          <Square value={4} />
-          <Square value={5} />
-          <Square value={0} />
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className="board-row">
-          <Square value={6} />
-          <Square value={7} />
-          <Square value={8} />
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
     </>
