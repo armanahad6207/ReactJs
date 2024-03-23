bdcf5361bde541b11bdedff8579f44d37c4fc1db
armanahad6207 <armanahad7549@gmail.com>
2024-03-23 20:11:11 +0530

    props

---
+++ 03tailwindandprops/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-red-500 rounded-sm p-3">heloo world</h1>
+    </>
+  );
+}
+
+export default App;
