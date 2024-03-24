334a0dd698fbda2e06b2349e11eae88e71d15189
armanahad6207 <armanahad7549@gmail.com>
2024-03-24 12:08:42 +0530

    update

--- 03tailwindandprops/src/App.jsx
+++ 03tailwindandprops/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Card from "./components/Card";
+const userdetails = {
+  name: "jhon",
+  age: 45,
+  gender: "male",
+};
 
 function App() {
   return (
     <>
       <h1 className="bg-green-400 text-red-500 rounded-sm p-3">heloo world</h1>
+      <Card userName="chewk" details={userdetails} />
+      <Card userName="zenifer" details={userdetails} />
+      <Card userName="lucifer" details={userdetails} />
     </>
   );
 }
---
+++ 03tailwindandprops/src/components/Card.jsx
@@ -0,0 +1,24 @@
+const Card = ({ userName }) => {
+  return (
+    <>
+      <div className="my-6 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img
+          className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+          src="https://images.pexels.com/photos/3974517/pexels-photo-3974517.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt="Woman's Face"
+        />
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">{userName}</p>
+            <p className="text-slate-500 font-medium">Product Engineer</p>
+          </div>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Message
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
