68d9b6ddae979a70dbd72b48cfdd0c6d0cf000ff
armanahad6207 <armanahad7549@gmail.com>
2024-06-05 13:40:45 +0530

    display fetch data inside table on screeen

--- productlist/src/App.js
+++ productlist/src/App.js
@@ -15,17 +15,57 @@ function App() {
   console.log(data);
 
   return (
-    <div className=" container bg-gray-200 p-5 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {data.map((val) => {
-        return (
-          <Product
-            key={val.id}
-            title={val.title}
-            price={val.price}
-            image={val.images[0]}
-          />
-        );
-      })}
+    <>
+      <Producttable data={data} />
+      <div className=" container bg-gray-200 p-5 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {data.map((val) => {
+          return (
+            <Product
+              key={val.id}
+              title={val.title}
+              price={val.price}
+              image={val.images[0]}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+function Producttable({ data }) {
+  return (
+    <div className="p-12">
+      <table
+        style={{
+          width: "100%",
+        }}
+      >
+        <thead>
+          <tr className="font-bold text-xl">
+            <td>id</td>
+            <td>title</td>
+            <td>price</td>
+            <td>image</td>
+          </tr>
+        </thead>
+        {data.map((item) => {
+          return (
+            <>
+              <tbody>
+                <tr className="text-[10px] mt-2 bg-yellow-400 border border-blue-400 overflow-hidden">
+                  <td className="bg-black text-white text-center ">
+                    {item.id}
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.price}</td>
+                  <td>{item.images[0]}</td>
+                </tr>
+              </tbody>
+            </>
+          );
+        })}
+      </table>
     </div>
   );
 }
