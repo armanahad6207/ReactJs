8ce7a2db6b168d49beee145115d232f6fca90ecf
armanahad6207 <armanahad7549@gmail.com>
2024-06-10 21:33:08 +0530

    update

--- zomato-app/src/Components/Search/Search.jsx
+++ zomato-app/src/Components/Search/Search.jsx
@@ -2,26 +2,15 @@ import bgImage from "../../assets/shutterstock_348320018@2x.png";
 function Search() {
   return (
     <>
-      <div className="container relative max-w-full h-[80vh] overflow-hidden">
-        <img src={bgImage} className="w-full h-full object-cover" />
-        <div className="absolute top-1/2 sm:top-1/2 sm:left-1/3 flex flex-col  gap-6">
-          <p className="text-xl sm:text-2xl  font-semibold text-white text-center">
-            Find the best restaurants, cafés, and bars
-          </p>
-          <div className="flex flex-col sm:flex-row justify-start gap-5 px-6 ">
-            <select className=" hover:bg-gray-200 bg-white rounded-sm text-sm px-3 py-4 text-gray-500 w-full sm:w-52 appearance-none outline-none  ">
-              <option className="">Please type a location</option>
-              <option>option1</option>
-              <option>option1</option>
-              <option>option1</option>
-            </select>
-            <select className=" hover:bg-gray-200 bg-white rounded-sm text-sm  text-gray-500 px-3 py-4 w-full appearance-none outline-none ">
-              <option>Search for restaurant</option>
-              <option>result</option>
-              <option>result</option>
-            </select>
-          </div>
-        </div>
+      <div
+        className="w-full h-[80vh] bg-slate-600   "
+        style={{
+          background: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className=" "></div>
       </div>
     </>
   );
